cfb0d5c4df0b4466788cb62a2ae7eb47ff55fb40
Bohdan Limanskyi <lyamzer2002@example.com>
2026-02-08 23:06:42 +0100

    feat: add NativeWind and configure Tailwind/Babel for react-native-reusables

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from 'react-native';
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>
+    <View className="pt-20">
       <Text>HomeScreen</Text>
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Button variant="default" className="w-40">
+        <Text>213</Text>
+      </Button>
+      <Button variant="default">
+        <Text>213</Text>
+      </Button>
     </View>
   );
 }
